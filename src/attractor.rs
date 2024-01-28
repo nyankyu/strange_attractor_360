a3309423f2f9f2d7d8ee21ab57b3046fd1085a5b
nyankyu <fukujinn@gmail.com>
2024-01-29 00:19:34 +0900

    display axis

--- src/attractor.rs
+++ src/attractor.rs
@@ -1,14 +1,16 @@
-mod particle;
 mod param;
+mod particle;
 
-#[allow(unused_imports)]
-pub(crate) use param::lorenz_attractor::LorenzAttractor;
+use nannou::glam::Vec3Swizzles;
+use nannou::prelude::*;
+
 #[allow(unused_imports)]
 pub(crate) use param::halvorsen_attractor::HalvorsenAttractor;
 #[allow(unused_imports)]
+pub(crate) use param::lorenz_attractor::LorenzAttractor;
+#[allow(unused_imports)]
 pub(crate) use param::thomas_attractor::ThomasAttractor;
 
-use nannou::prelude::*;
 use particle::Particle;
 
 const SCALE: f32 = crate::WINDOW_H as f32 / PI;
@@ -54,7 +56,9 @@ impl<Param: AttractorParam> Attractor<Param> {
     pub(crate) fn new() -> Self {
         Attractor {
             _param: Param::new(),
-            orbits: (0..Param::ORBIT_NUM).map(|_| Particle::new::<Param>()).collect(),
+            orbits: (0..Param::ORBIT_NUM)
+                .map(|_| Particle::new::<Param>())
+                .collect(),
             theta: 0.0,
             rotation: Mat3A::ZERO,
             camera: vec3a(Param::CAMERA_X, Param::CAMERA_Y, Param::CAMERA_Z),
@@ -77,6 +81,43 @@ impl<Param: AttractorParam> Attractor<Param> {
         self.orbits.iter().for_each(|particle| {
             particle.draw::<Param>(&draw, self.rotation, self.center, self.camera)
         });
+        if !crate::RECORDING {
+            self.draw_axis(&draw);
+        }
+    }
 
+    fn draw_axis(&self, draw: &Draw) {
+        let origin = self.coordinate(&Vec3A::ZERO);
+        let axis_x = self.coordinate(&vec3a(500.0, 0.0, 0.0));
+        let axis_y = self.coordinate(&vec3a(0.0, 500.0, 0.0));
+        let axis_z = self.coordinate(&vec3a(0.0, 0.0, 500.0));
+
+        draw.line()
+            .start(origin)
+            .end(axis_x)
+            .weight(0.002)
+            .color(BLUE);
+
+        draw.line()
+            .start(origin)
+            .end(axis_y)
+            .weight(0.002)
+            .color(GREEN);
+
+        draw.line()
+            .start(origin)
+            .end(axis_z)
+            .weight(0.002)
+            .color(WHITE);
+    }
+
+    fn coordinate(&self, p: &Vec3A) -> Vec2 {
+        let rotated = self.rotation * (*p - self.center);
+        let dist_xy = self.camera.xy().distance(rotated.xy());
+        let longitude =
+            ((rotated.x - self.camera.x) / dist_xy).acos() * (rotated.y - self.camera.y).signum();
+        let latitude = ((rotated.z - self.camera.z) / dist_xy).atan();
+
+        vec2(longitude, latitude)
     }
 }
